675eb315aacbd986110e333397beab701c581e49
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-05-03 19:16:25 +0600

    hsd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,77 @@
 <template>
   <div class="home">
     <h1>Select a template</h1>
-    <v-container class="dfcsdcssd">
-      <v-card class="mx-auto" max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-        >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
-        </v-img>
-      </v-card>
-    </v-container>
+    <v-row class="mb-6" no-gutters>
+      <v-col cols="12" md="4">
+        <router-link to="/template1">
+          <div class="template">
+            <img src="/img/template1.png" alt="" />
+          </div>
+        </router-link>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <router-link to="/template2">
+          <div class="template">
+            <img src="/img/template2.png" alt="" />
+          </div>
+        </router-link>
+      </v-col>
+
+      <!-- <v-col cols="12" md="4">
+        <router-link to="/template3">
+          <div class="template">
+            <img src="/img/template3.png" alt="" />
+          </div>
+        </router-link>
+      </v-col> -->
+
+                     <v-col cols="12" md="4">
+        <router-link to="/ramadan">
+          <div class="template">
+            <img src="/img/1-01-01.jpg" alt="" />
+          </div>
+        </router-link>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <router-link to="/template4">
+          <div class="template">
+            <img src="/img/template4.png" alt="" />
+          </div>
+        </router-link>
+      </v-col>
+       <v-col cols="12" md="4">
+        <router-link to="/template5">
+          <div class="template">
+            <img src="/img/template3.png" alt="" />
+          </div>
+        </router-link>
+      </v-col>
+
+
+             <v-col cols="12" md="4">
+        <router-link to="/template6">
+          <div class="template">
+            <img src="/img/template3.png" alt="" />
+          </div>
+        </router-link>
+
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+
+
+
 </script>
 
 <style>
-.csccsc {
-  width: 30px;
-  height: 30px;
+img {
+  max-width: 100%;
 }
 
 .template {
@@ -35,80 +84,4 @@
   box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
     0 1px 5px 0 rgba(0, 0, 0, 0.12);
 }
-
-.dyswdgw {
-  width: 40px;
-  height: 40px;
-}
-
-.v-button {
-  font-size: 12px;
-  height: 30px;
-  text-align: center;
-}
-
-.dfcsdcssd {
-  border: ;
-}
 </style>
-
-//
-<v-row class="mb-6" no-gutters>
-  //     <v-col cols="12" md="4">
-  //       <router-link to="/template1">
-  //         <div class="template">
-  //           <img src="/img/template1.png" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col>
-
-  //     <v-col cols="12" md="4">
-  //       <router-link to="/template2">
-  //         <div class="template">
-  //           <img src="/img/template2.png" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col>
-
-  //     <!-- <v-col cols="12" md="4">
-  //       <router-link to="/template3">
-  //         <div class="template">
-  //           <img src="/img/template3.png" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col> -->
-
-  //                    <v-col cols="12" md="4">
-  //       <router-link to="/ramadan">
-  //         <div class="template">
-  //           <img src="/img/1-01-01.jpg" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col>
-
-  //     <v-col cols="12" md="4">
-  //       <router-link to="/template4">
-  //         <div class="template">
-  //           <img src="/img/template4.png" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col>
-  //      <v-col cols="12" md="4">
-  //       <router-link to="/template5">
-  //         <div class="template">
-  //           <img src="/img/template3.png" alt="" />
-  //         </div>
-  //       </router-link>
-  //     </v-col>
-
-
-  //            <v-col cols="12" md="4">
-  //       <router-link to="/template6">
-  //         <div class="template">
-  //           <img src="/img/template3.png" alt="" />
-  //         </div>
-  //       </router-link>
-
-  //     </v-col>
-      
-  //   </v-row>
